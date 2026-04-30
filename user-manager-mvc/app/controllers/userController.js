821cccd3484f5d14ce36a7e2c4bcb699206313d3
bellafkih.22128 <bellafkih.deyae.studente@itispaleocapa.it>
2026-04-30 08:56:40 +0200

    aggiunta immagine

--- user-manager-mvc/app/controllers/userController.js
+++ user-manager-mvc/app/controllers/userController.js
@@ -1,19 +1,16 @@
-// app/controllers/userController.js
-const User = require('../models/userModel');
-exports.home = (req, res) => {
-  res.render('index');
+// app/models/userModel.js
+let users = [];
+const User = {
+  findAll: () => users,
+  create: (name, email, linkImg) => {
+    const newUser = { id: Date.now(), name, email, linkImg };
+    users.push(newUser);
+    return newUser;
+  },
+  findById: (id) => users.find(u => u.id === parseInt(id)),
+  delete: (id) => {
+    users = users.filter(u => u.id !== parseInt(id));
+  }
 };
-exports.listUsers = (req, res) => {
-  const users = User.findAll();
-  res.render('users', { users });
-};
-exports.createUser = (req, res) => {
-  const { name, email } = req.body;
-  User.create(name, email);
-  res.redirect('/users');
-};
-exports.deleteUser = (req, res) => {
-  const { id } = req.params;
-  User.delete(id);
-  res.redirect('/users');
-};
+
+module.exports = User;
